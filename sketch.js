8a595d317bddb76f33196116887788bf8bda8895
RAHUL <56194955+RAHULTATA10@users.noreply.github.com>
2019-12-07 14:12:46 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,13 +3,13 @@ var rect1, rect2 ;
 function setup() {
     createCanvas(400, 400) ;
 
-    rect1 = createSprite(120, 240, 90, 42) ;
+    rect1 = createSprite(150, 280, 90, 42) ;
     
     rect1.shapeColor = "red";
     rect1.setCollider("Circle", 0, 0, 75) ;
     rect1.debug = true ; 
 
-    rect2 = createSprite(280, 160, 37, 87) ;
+    rect2 = createSprite(320, 180, 37, 87) ;
     
     rect2.shapeColor = "blue";
     rect2.setCollider("Circle", 0, 0, 75) ;
@@ -17,7 +17,7 @@ function setup() {
 }
 
 function draw() {
-    background (303, 250,203) ;
+    background (303, 250, 203) ;
 
         rect2.x = World.mouseX ;
         rect2.y = World.mouseY ;
